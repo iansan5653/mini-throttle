2b5402701d49ecf72963471afa9b2c9f587ac891
Ian Sanders <iansan5653@github.com>
2026-07-23 15:46:34 +0000

    Fix middle behavior near start of sequence

--- index.ts
+++ index.ts
@@ -28,6 +28,7 @@ export function throttle<T extends unknown[]>(
   wait = 0,
   {start = true, middle = true, once = false, end = true}: ThrottleOptions = {}
 ): Throttler<T> {
+  /** Time of last made call in this sequence, or time of start of sequence if a call hasn't been made yet. */
   let lastCall = 0
   let lastAttempt = 0
   let endTimer: ReturnType<typeof setTimeout>
@@ -46,6 +47,8 @@ export function throttle<T extends unknown[]>(
     const isStartOfSequence = Date.now() - lastAttempt >= wait
     lastAttempt = Date.now()
 
+    if (isStartOfSequence) lastCall = Date.now()
+
     // start: If it has been a sufficient time since the last attempt, start a new sequence
     if (start && isStartOfSequence) {
       makeCall()
@@ -53,7 +56,7 @@ export function throttle<T extends unknown[]>(
     }
 
     // middle: If it has been sufficient time since the last successful call, make another call
-    if (middle && Date.now() - lastCall >= wait && !isStartOfSequence) {
+    if (middle && Date.now() - lastCall >= wait) {
       makeCall()
       return
     }
